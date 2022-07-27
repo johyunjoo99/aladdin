92b76add438b0889e79240e9cc92feef33072219
johyunjoo99 <chj17200@naver.com>
2022-07-27 10:15:44 +0900

    Aladdin_2022.07.27(4)
    txt 경로 수정

--- script/purchase_txt.js
+++ script/purchase_txt.js
@@ -3,12 +3,14 @@ $(function(){
     //세일즈 포인트 소개글
     $.get("./txt/sales_point.txt", function(data){
         $("#score .textbox").html(data);
+
+        // 소개글 창 닫기
+        $(".textbox .fa-xmark").click(function () {
+            $("#score .textbox").css("display", "none");
+        });
     })
 
-    // 소개글 창 닫기
-    $(".textbox .fa-xmark").click(function () {
-        $("#score .textbox").css("display", "none");
-    });
+
 
 
     // 책 소개글 텍스트 삽입
@@ -24,7 +26,7 @@ $(function(){
 
 
     // 배송 / 교환 안내 텍스트 삽입
-    $.get("../txt/delivery_infomation.txt", function(data){
+    $.get("./txt/delivery_infomation.txt", function(data){
         $("#delivery").html(data);
     })
 
@@ -32,11 +34,11 @@ $(function(){
     //글 작성 시 유의사항
     $.get("./txt/write_notice.txt", function(data){
         $(".review_star .textbox").html(data);
-    })
 
-    // 글 작성 시 유의사항 창 닫기
-    $(".review_star .fa-xmark").click(function(){
-        $(".review_star .textbox").css("display", "none");
+        // 글 작성 시 유의사항 창 닫기
+        $(".review_star .fa-xmark").click(function () {
+            $(".review_star .textbox").css("display", "none");
+        })
     })
 
 });
